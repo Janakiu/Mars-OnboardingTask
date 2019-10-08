9d72f93eceb0905329b02af83245c3db67dbcfdd
Janaki <janakiu3@gmail.com>
2019-10-09 01:00:37 +1100

    First Commit

---
+++ SpecflowPages/Utils/ConstantUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpecflowPages
+{
+    public class ConstantUtils
+    {
+        //Base Url
+        public static string Url = "http://www.skillswap.pro/";
+
+        //ScreenshotPath
+        public static string ScreenshotPath = @"C:\Users\User\Downloads\SpecflowTests-Base\SpecflowPages\TestReports\Screenshots";
+       
+
+        //ExtentReportsPath
+        public static string ReportsPath = @"C:\Users\User\Downloads\SpecflowTests-Base\SpecflowPages\TestReports\Test.html";
+        
+
+
+        //ReportXML Path
+        public static string ReportXMLPath = @"C:\Users\User\Downloads\SpecflowTests-Base\SpecflowPages\TestReports\ReportXML.xml";
+        
+
+
+    }
+}
